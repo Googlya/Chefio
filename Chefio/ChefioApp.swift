f5d2d98a563f4f121b1e71cd0e95436ca64d7994
Alexandr Farnosov <alexandr.farnosov1@gmail.com>
2022-05-19 14:44:32 +0500

    Initial Commit

---
+++ Chefio/ChefioApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChefioApp.swift
+//  Chefio
+//
+//  Created by Александр Фарносов on 19.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct ChefioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Chefio/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Chefio
+//
+//  Created by Александр Фарносов on 19.05.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
